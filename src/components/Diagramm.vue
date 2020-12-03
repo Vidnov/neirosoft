83003ac4ea47b016f5a085991769dcb313127752
Vidnov <44727266+Vidnov@users.noreply.github.com>
2020-12-03 14:55:44 +0300

    85% complite

--- src/components/Diagramm.vue
+++ src/components/Diagramm.vue
@@ -1,31 +1,72 @@
 
 <script>
 import VueChartJs from "vue-chartjs";
-//const { reactiveProp } = VueChartJs.mixins;
 export default {
   name: "Diagramm",
   extends: VueChartJs.Doughnut,
   components: {},
- 
-  props: ["departments","employees","color"],
+  data() {
+    return {
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        layout: {
+          padding: {
+            left: -430,
+            right: 0,
+            top: 0,
+            bottom: 0,
+          },
+        },
+        elements: {
+          point: {
+            radius: 3,
+            pointStyle: "circle",
+          },
+        },
+        title: {},
+        legend: {
+          align: "center",
+          position: "right",
+          fullWidth: true,
+          labels: {
+            usePointStyle: true,
+            boxWidth: 35,
+            fontSize: 22,
+            padding: 30,
+          },
+        },
+      },
+    };
+  },
+
+  props: ["departments", "employees", "color"],
 
   mounted() {
     this.renderChart(
       {
-       labels: '',
+        labels: this._props.departments,
         datasets: [
           {
-            label: this._props.employees,
+            label: this._props.departments,
             backgroundColor: this._props.color,
             data: this._props.employees,
           },
         ],
+        options: {
+          legend: {
+            position: "right",
+          },
+        },
       },
-      { responsive: true, maintainAspectRatio: true }
+      this.options
     );
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
+.chartjs-size-monitor-shrink {
+  background-color: black;
+}
 </style>
